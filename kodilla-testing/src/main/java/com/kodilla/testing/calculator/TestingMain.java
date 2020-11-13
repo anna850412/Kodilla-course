7578cbe33e43a9d63957a7ca9ec998946ca84437
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2020-11-13 12:59:24 +0100

    6.2. Calculator first unit test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.sql.SQLOutput;
+
+public class TestingMain {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator(7,1);
+        int result1 = calculator.addAtoB(5,3);
+        int result2 = calculator.substractAfromB(2,1);
+        if(result1 == (8) && result2 == (1)) {
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("Error");
+        }
+    }
+}
