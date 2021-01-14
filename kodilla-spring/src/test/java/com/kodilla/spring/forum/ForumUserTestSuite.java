3178c1a82e4c7be772f72e1e0f56ffe22d1a61fe
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2021-01-14 11:40:17 +0100

    14.1 bean ForumUser changes in class test

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -2,9 +2,11 @@ package com.kodilla.spring.forum;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class ForumUserTestSuite {
     @Test
     void testGetUsername(){
