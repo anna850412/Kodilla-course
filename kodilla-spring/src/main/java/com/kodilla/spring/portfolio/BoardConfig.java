310d8971e4ab57cf1f779b67ce976dbc811484dc
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2021-01-14 17:31:36 +0100

    kodilla-spring/src/main/java/com/kodilla/spring/portfolio

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,2 +1,37 @@
-package com.kodilla.spring.portfolio;public class BoardConfig {
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+    @Autowired
+//    @Qualifier("toDo")
+    Board board;
+
+    @Bean
+    Board getBoard(){
+        return new Board();
+    }
+
+    @Bean(name = "inProgress")
+    @Scope("prototype")
+    public TaskList getInProgressList(){
+        return new Board().getInProgressList();
+    }
+    @Bean(name = "toDo")
+    @Scope("prototype")
+    public TaskList getToDoList(){
+        return new Board().getToDoList();
+    }
+
+    @Bean(name = "done")
+    @Scope("prototype")
+    public TaskList getDoneList(){
+        return new Board().getDoneList();
+    }
+
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -1,2 +1,17 @@
-package com.kodilla.spring.portfolio;public class TaskList {
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+    private List<String> tasks;
+
+    public TaskList(List<String> tasks) {
+        this.tasks = new ArrayList<>();
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
 }
