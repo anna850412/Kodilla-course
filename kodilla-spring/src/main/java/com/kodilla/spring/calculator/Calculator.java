a568c7dc61062dea521f44d0cc081cd236dbc680
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2021-01-15 09:54:28 +0100

    version which display result

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -4,27 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public final class Calculator {
+public class Calculator {
     @Autowired
     private Display display;
-    double val;
 
     public double add(double a, double b) {
-        display.displayValue(a);
-        display.displayValue(b);
-        return val = a + b;
+        double val = a + b;
+        display.displayValue(val);
+        return val;
+
     }
 
     public double sub(double a, double b) {
         display.displayValue(a);
         display.displayValue(b);
-        return a - b;
+        return  a - b;
     }
 
     public double mul(double a, double b) {
         display.displayValue(a);
         display.displayValue(b);
-        return a * b;
+        return  a * b;
+
     }
 
     public double div(double a, double b) {
