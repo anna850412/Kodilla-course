f02837bf57079b711f2fc8a50f4cb4ca561e73c7
Anna Kołodziejczyk <anna.ak.kolodziejczyk@gmail.com>
2021-02-03 11:39:03 +0100

    sudoku

---
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuBoard.java
@@ -0,0 +1,10 @@
+package com.kodilla.sudoku;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SudokuBorad {
+    String pipe = "|";
+    String 
+    List<SudokuRow> sudokuRowList = new ArrayList<>();
+}
