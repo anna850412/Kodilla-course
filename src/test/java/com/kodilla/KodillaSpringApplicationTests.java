032acd2b50be8c0d7b7864a3eabc5c1f93f8744d
Pawel Pluta <pluta.p@gmail.com>
2021-02-04 20:16:46 +0100

    Fix structure

---
+++ src/test/java/com/kodilla/KodillaSpringApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KodillaSpringApplicationTests {
+
+    @Test
+    public void contextLoad() {}
+}
